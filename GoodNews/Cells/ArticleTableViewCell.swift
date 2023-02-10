28696a76fe2dbe259544497096249ff7fb1bc831
Kris Peng <kris19951112@gmail.com>
2023-02-10 16:41:08 +0800

    feat(cell): add table view cell

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by Kris on 2023/2/8.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
